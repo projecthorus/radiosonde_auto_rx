1c468a7d780e84fd21f09f9eddeeb4684c53514d
Zilog80 <rs1729@users.noreply.github.com>
2016-10-26 16:38:24 +0200

    rs92 output -> kml

---
+++ rs92/pos2kml.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my $filename = $ARGV[0];
+my $fh;
+open($fh, $filename) or die "Could not open $filename: $!";
+
+print "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n";
+print "<kml xmlns=\"http://www.opengis.net/kml/2.2\">\n";
+
+my $line;
+my $hms;
+my $lat; my $lon; my $alt;
+
+print "  <Document>\n";
+print "    <Placemark>\n";
+print "      <LineString>\n";
+print "      <altitudeMode>absolute</altitudeMode>\n";
+print "        <coordinates>\n";
+print "           ";
+while ($line = <$fh>) {
+    if ($line =~ /(\d\d:\d\d:\d\d).*\ lat:\ *(-?\d*\.\d*)\ +lon:\ *(-?\d*\.\d*)\ +alt:\ *(-?\d*\.\d*).*/) {
+        $hms = $1;
+        $lat = $2;
+        $lon = $3;
+        $alt = $4;
+        print  " $lon,$lat,$alt";
+    }
+}
+print "\n";
+print "        </coordinates>\n";
+print "      </LineString>\n";
+print "    </Placemark>\n";
+print "  </Document>\n";
+
+print "</kml>\n";
+
