9c7202cd4fc3da906789c03f886fb525062a6a54
Viproz <viproz12@gmail.com>
2018-12-19 21:58:06 +0100

    Changed badnwidth for M10

--- auto_rx/autorx/decode.py
+++ auto_rx/autorx/decode.py
@@ -246,8 +246,8 @@ class SondeDecoder(object):
         elif self.sonde_type == "M10":
             # M10 Sondes
 
-            decode_cmd = "%s %s-p %d -d %s %s-M fm -F9 -s 15k -f %d 2>/dev/null |" % (self.sdr_fm, bias_option, int(self.ppm), str(self.device_idx), gain_param, self.sonde_freq)
-            decode_cmd += "sox -t raw -r 15k -e s -b 16 -c 1 - -r 48000 -b 8 -t wav - highpass 20 2>/dev/null |"
+            decode_cmd = "%s %s-p %d -d %s %s-M fm -F9 -s 22k -f %d 2>/dev/null |" % (self.sdr_fm, bias_option, int(self.ppm), str(self.device_idx), gain_param, self.sonde_freq)
+            decode_cmd += "sox -t raw -r 22k -e s -b 16 -c 1 - -r 48000 -b 8 -t wav - highpass 20 2>/dev/null |"
             # M10 decoder
             decode_cmd += "./m10 -b 2>/dev/null"
 
