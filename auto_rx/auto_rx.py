24388e8893c3e505440902769b9f71ddabb6ff55
musti <musti@wlan-si.net>
2018-12-24 21:07:53 +0100

    updated paths for balena

--- auto_rx/auto_rx.py
+++ auto_rx/auto_rx.py
@@ -364,7 +364,7 @@ def main():
 
     # Command line arguments. 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c" ,"--config", default="station.cfg", help="Receive Station Configuration File. Default: station.cfg")
+    parser.add_argument("-c" ,"--config", default="/data/station.cfg", help="Receive Station Configuration File. Default: station.cfg")
     parser.add_argument("-f", "--frequency", type=float, default=0.0, help="Sonde Frequency Override (MHz). This overrides the scan whitelist with the supplied frequency.")
     parser.add_argument("-t", "--timeout", type=int, default=0, help="Close auto_rx system after N minutes. Use 0 to run continuously.")
     parser.add_argument("-v", "--verbose", help="Enable debug output.", action="store_true")
