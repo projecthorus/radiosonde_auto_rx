c935f32db31f76be473371ab60525ca77991c06f
Mark Jessop <vk5qi@rfhead.net>
2019-02-02 15:42:16 +1030

    Updated version.

--- auto_rx/autorx/__init__.py
+++ auto_rx/autorx/__init__.py
@@ -5,7 +5,7 @@
 #   Copyright (C) 2018  Mark Jessop <vk5qi@rfhead.net>
 #   Released under GNU GPL v3 or later
 #
-__version__ = "20190131-beta"
+__version__ = "20190202-beta"
 
 # Global Variables
 
--- auto_rx/build.sh
+++ auto_rx/build.sh
@@ -16,6 +16,7 @@ gcc rs92dm_dft.c demod_dft.o -lm -o rs92ecc -I../ecc/ -I../rs92
 gcc rs41dm_dft.c demod_dft.o -lm -o rs41ecc -I../ecc/ -I../rs41
 gcc dfm09dm_dft.c demod_dft.o -lm -o dfm09ecc -I../ecc/ -I../dfm
 # Build M10 decoder
+echo "Building M10 Demodulator."
 cd ../m10/
 g++ M10.cpp M10Decoder.cpp M10GeneralParser.cpp M10GTopParser.cpp M10PtuParser.cpp AudioFile.cpp -lm -o m10 -std=c++11
 
