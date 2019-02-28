820b0a6ffd686c0b6efa13827be269a763880f5b
Mark Jessop <vk5qi@rfhead.net>
2019-02-28 20:01:20 +1030

    Update version of testing branch.

--- auto_rx/autorx/__init__.py
+++ auto_rx/autorx/__init__.py
@@ -5,7 +5,7 @@
 #   Copyright (C) 2018  Mark Jessop <vk5qi@rfhead.net>
 #   Released under GNU GPL v3 or later
 #
-__version__ = "20190227-beta"
+__version__ = "20190228-beta"
 
 # Global Variables
 
