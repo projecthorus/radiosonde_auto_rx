7daffbb567238899f98a853ccd4ff45ad713b129
vg1320 <vgeslin@free.fr>
2020-03-11 20:29:44 +0100

    more unique version number

--- auto_rx/autorx/__init__.py
+++ auto_rx/autorx/__init__.py
@@ -17,7 +17,7 @@ except ImportError:
 # MINOR - New sonde type support, other fairly big changes that may result in telemetry or config file incompatability issus.
 # PATCH - Small changes, or minor feature additions.
 
-__version__ = "1.3.0~fork.0.0.1"
+__version__ = "1.3.0-vg1320"
 
 
 # Global Variables
