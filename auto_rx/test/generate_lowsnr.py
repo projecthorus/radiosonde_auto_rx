b664553d96bb411bfcb052f604b7d9200fd34911
Mark Jessop <vk5qi@rfhead.net>
2020-06-09 08:10:56 +0930

    Fix LMS6-400 baudrate in generate_lowsnr

--- auto_rx/test/generate_lowsnr.py
+++ auto_rx/test/generate_lowsnr.py
@@ -43,7 +43,7 @@ SAMPLES = [
     ['m10_96k_float.bin', 9616, -10.0, 96000],  # Really weird baud rate.
     ['imet4_96k_float.bin', 1200, -10.0, 96000], # 1200 baud, but AFSK, so we expect 7-8 dB worse performance than the other sondes.
     ['rsngp_96k_float.bin', 2400, -100.0, 96000], # RS92-NGP - wider bandwidth.
-    ['lms6-400_96k_float.bin', 9600, -100, 96000] # LMS6, 400 MHz variant. Continuous signal.
+    ['lms6-400_96k_float.bin', 4800, -100, 96000] # LMS6, 400 MHz variant. Continuous signal.
 ]
 
 
