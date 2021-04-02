c546c75d6b7dea951a9afe4459531f47b8f96d10
F4HVQ <seb@Leonardo.CARTOONS>
2021-04-02 15:19:55 +0000

    [UPD] Calculate statistics on all log files and all records [ADD] lib files [UDP] Move README file to documentation folder [UDP] Move cfg file to configuration folder [ADD] Add INSTALL file

--- auto_rx/utils/receiver_stats.py
+++ auto_rx/utils/receiver_stats.py
@@ -1,23 +1,31 @@
-#!/usr/bin/env python
+
+#!/bin/python3
+
 #
 # Receiver Statistics Calculator
 #
 # 2018-04 Mark Jessop <vk5qi@rfhead.net>
 #
-# Usage:
-# python receiver_stats.py --lat=yourlat --lon=yourlon --alt=youralt ../log/
-# lat and lon are in decimal degrees.
-#
+
+
+# Global imports
 import argparse
-import glob
-import numpy as np
-import sys
 from math import radians, degrees, sin, cos, atan2, sqrt, pi
+from pathlib import Path
+
+# Specific imports
+from libs.CsvHandler import CsvHandler
+from libs.FileSystem import get_file_list
 
 
+FILE_NAME = Path(__file__).name
+
+
+# Functions definitions
 # Earthmaths code by Daniel Richman (thanks!)
 # Copyright 2012 (C) Daniel Richman; GNU GPL 3
-def position_info(listener, balloon):
+def position_info(listener: tuple,
+                  balloon: tuple) -> dict:
     """
     Calculate and return information from 2 (lat, lon, alt) tuples
 
@@ -34,8 +42,8 @@ def position_info(listener, balloon):
     """
 
     # Earth:
-    #radius = 6371000.0
-    radius = 6364963.0 # Optimized for Australia :-)
+    # radius = 6371000.0
+    radius = 6364963.0  # Optimized for Australia :-)
 
     (lat1, lon1, alt1) = listener
     (lat2, lon2, alt2) = balloon
@@ -99,81 +107,125 @@ def position_info(listener, balloon):
     }
 
 
+# Classes definition
+class ReceiverStats(object):
+    """ ReceiverStats class """
+    def __init__(self,
+                 station_lat : float,
+                 station_lon : float,
+                 station_alt : float,
+                 log_folder : Path) -> None:
 
-def read_last_position(filename):
-    ''' Read in a a sonde log file, and return the last lat/lon/alt '''
+        self._station_lat = station_lat
+        self._station_lon = station_lon
+        self._station_alt = station_alt
+        self._log_folder  = log_folder
 
-    _last = None
-    _f = open(filename, 'r')
-    for line in _f:
-        _last = line
-    
-    return _last.split(',')
+    def calculate_stats(self):
+        """ Calculate statistics for all file """
+
+        log_file_list = get_file_list(self._log_folder, "*.log")
+        for counter, csv_file in enumerate(log_file_list):
+            print("\n{}/{}\tAnalysing '{}'".format(counter+1, len(log_file_list), csv_file))
+            csv_file_data = CsvHandler(csv_file)
+            if len(csv_file_data) > 0:
+                statistics = {
+                    'furthest': {'range': 0},
+                    'lowest': {'elev': 90}
+                }
+
+                for record in csv_file_data:
+
+                    ballon_lat = float(record['lat'])
+                    ballon_lon = float(record['lon'])
+                    ballon_alt = float(record['alt'])
+
+                    _stats = position_info((self._station_lat, self._station_lon, self._station_alt),
+                                           (ballon_lat, ballon_lon, ballon_alt))
+
+                    _range = _stats['straight_distance'] / 1000.0
+                    _elev  = _stats['elevation']
+
+                    # Note : Get stats for furthest range
+                    if _range > statistics['furthest']['range']:
+                        # Update the furthest distance
+                        statistics['furthest']['range']   = _range
+                        statistics['furthest']['elev']    = _stats['elevation']
+                        statistics['furthest']['bearing'] = _stats['bearing']
+                        # statistics['furthest']['telem']   = last_record
+
+                    # Note : Get stats for lowest elevation
+                    if _elev < statistics['lowest']['elev']:  # ) and (_range > 20):
+                        statistics['lowest']['range']   = _range
+                        statistics['lowest']['elev']    = _stats['elevation']
+                        statistics['lowest']['bearing'] = _stats['bearing']
+                        # statistics['lowest']['telem']   = last_record
+
+                #  Note : Print stats for current file
+                print("\t\tLongest distance: {:0.3f} km".format(statistics['furthest']['range']))
+                print("\t\t\tDetail: {:0.3f} deg elevation, {:0.3f} deg bearing".format(statistics['furthest']['elev'], statistics['furthest']['bearing']))
+                # print("\t\t\tLast Telemetry: {:0.3f}".format(statistics['furthest']['telem']))
+
+                print("\t\tLowest elevation: {:0.3f} degrees".format(statistics['lowest']['elev']))
+                print("\t\t\tDetail: {:0.3f} km range, {:0.3f} deg bearing".format(statistics['lowest']['range'], statistics['lowest']['bearing']))
+                # print("\t\t\tLast Telemetry: {:0.3f}".format(statistics['lowest']['telem']))
+
+                nb_frame_received = len(csv_file_data) - 1
+                frame_count_begin = int(csv_file_data[0]['frame'])
+                frame_count_end   = int(csv_file_data[nb_frame_received]['frame'])
+                frame_count_diff  = frame_count_end - frame_count_begin
+
+                print("\t\tRecords")
+                # print("\t\t\tBegin frame counter        : {}".format(frame_count_begin))
+                # print("\t\t\tEnd frame counter          : {}".format(frame_count_end))
+                print("\t\t\tDiff frame counter         : {}".format(frame_count_diff))
+                print("\t\t\tNumber of records received : {}".format(nb_frame_received))
+                print("\t\t\tRecords received           : {:0.1f} %".format((nb_frame_received/frame_count_diff)*100.0))
+
+            else:
+                print("ERROR : Log file is empty")
+
+
+def main():
+    """ Main entry point """
+
+    # Messages
+    epilog_msg = '''\
+        See above examples
+            - [?]$ ./{0} --lat 48.0 --lon 2.0 --alt 50.0 ../log/
+        '''.format(FILE_NAME)
+
+    # Specify command arguments
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description="This script generate some statistics of the receiver from station's log",
+                                     epilog=epilog_msg)
+
+    parser.add_argument("--lat", "--station_lat",
+                        default=48.90990378837107,
+                        type=float,
+                        help="Receiver latitude (unit : decimal degrees)")
+
+    parser.add_argument("--lon", "--station_lon",
+                        default=2.172286544644864,
+                        type=float,
+                        help="Receiver longitude (unit : decimal degrees)")
+
+    parser.add_argument("--alt", "--station_alt",
+                        default=50.0,
+                        type=float,
+                        help="Receiver altitude (unit : meter)")
+
+    parser.add_argument("folder",
+                        default="../log/",
+                        help="Folder containing log files")
+
+    args = parser.parse_args()
+
+    ReceiverStats(args.lat,
+                  args.lon,
+                  args.alt,
+                  Path(args.folder)).calculate_stats()
 
 
 if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--lat", default=-34.0, type=float, help="Receiver Latitude")
-    parser.add_argument("--lon", default=138.0, type=float, help="Receiver Longitude")
-    parser.add_argument("--alt", default=0.0, type=float, help="Receiver Altitude")
-    parser.add_argument("folder", default="../log/", help="Folder containing log files.")
-    args = parser.parse_args()
-
-
-    _file_list = glob.glob(args.folder + "*_sonde.log")
-    #print(_file_list)
-
-
-    statistics = {
-        'furthest': {'range':0},
-        'lowest': {'elev':90}
-        }
-
-
-    for _file in _file_list:
-        try:
-            _last_entry = read_last_position(_file)
-        except Exception as e:
-            print("Error processing file %s: " % _file + str(e))
-            continue
-
-        _last_pos = (float(_last_entry[3]),float(_last_entry[4]),float(_last_entry[5]))
-
-        _stats = position_info((args.lat, args.lon, args.alt), _last_pos)
-
-        _range = _stats['straight_distance']/1000.0
-        _elev = _stats['elevation']
-
-        if _range > statistics['furthest']['range']:
-            # Update the furthest distance
-            statistics['furthest']['range'] = _range
-            statistics['furthest']['elev'] = _stats['elevation']
-            statistics['furthest']['bearing'] = _stats['bearing']
-            statistics['furthest']['telem'] = _last_entry
-
-        if (_elev < statistics['lowest']['elev']) and (_range > 20) :
-            statistics['lowest']['range'] = _range
-            statistics['lowest']['elev'] = _stats['elevation']
-            statistics['lowest']['bearing'] = _stats['bearing']
-            statistics['lowest']['telem'] = _last_entry
-
-    print("Longest Distance: %.1f km" % statistics['furthest']['range'])
-    print("    Detail: %.1f deg elev, %.1f deg bearing" % (
-        statistics['furthest']['elev'],
-        statistics['furthest']['bearing']))
-    print("    Last Telemetry: %s" % str(",".join(statistics['furthest']['telem'])))
-    
-
-    print("Lowest Elevation: %.1f degrees" % statistics['lowest']['elev'])
-    print("    Detail: %.1f km range, %.1f deg bearing" % (
-        statistics['lowest']['range'],
-        statistics['lowest']['bearing']))
-    print("    Last Telemetry: %s" % str(",".join(statistics['lowest']['telem'])))
-
-
-
-
-
-
-
+    main()
