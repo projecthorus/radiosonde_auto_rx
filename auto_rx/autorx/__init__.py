4de3c5d4fb121c4b88c7d1558a829f956ed0bf53
Mark Jessop <vk5qi@rfhead.net>
2021-07-05 17:59:49 +0930

    Update main webpage title with station call

--- auto_rx/autorx/__init__.py
+++ auto_rx/autorx/__init__.py
@@ -17,7 +17,7 @@ except ImportError:
 # MINOR - New sonde type support, other fairly big changes that may result in telemetry or config file incompatability issus.
 # PATCH - Small changes, or minor feature additions.
 
-__version__ = "1.5.4"
+__version__ = "1.5.5-beta1"
 
 
 # Global Variables
