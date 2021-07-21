199e3a4503626170753f4a489c9a9859c58790b7
Mark Jessop <vk5qi@rfhead.net>
2021-07-21 22:51:53 +0930

    Wrap aprsid generation code in try/except, bump testing version

--- auto_rx/autorx/__init__.py
+++ auto_rx/autorx/__init__.py
@@ -17,7 +17,7 @@ except ImportError:
 # MINOR - New sonde type support, other fairly big changes that may result in telemetry or config file incompatability issus.
 # PATCH - Small changes, or minor feature additions.
 
-__version__ = "1.5.5-beta2"
+__version__ = "1.5.5-beta3"
 
 
 # Global Variables
