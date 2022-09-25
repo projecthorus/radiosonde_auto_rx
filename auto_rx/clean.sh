87fda79c40af963f4e073f016cefa1b3b5b8c832
Mark Jessop <darkside@Marks-MacBook-Pro.local>
2022-09-25 10:18:10 +0930

    Update demodulators from upstream repo

--- auto_rx/clean.sh
+++ auto_rx/clean.sh
@@ -12,15 +12,12 @@ echo "Removing binaries in the auto_rx directory."
 cd ../auto_rx/
 rm dft_detect
 rm fsk_demod
-rm imet1rs_dft
 rm imet4iq
-rm mk2a_lms1680
 rm mk2a1680mod
 rm rs41mod
 rm rs92mod
 rm dfm09mod
 rm m10mod
-rm mXXmod
 rm m20mod
 rm lms6Xmod
 rm meisei100mod
